315ff90c0c7f10f70e53cb3158124b0b1eee4370
Philex Fertich <philex.live@outlook.com>
2026-04-20 22:01:39 +0300

    States and Tokens enumerators added.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+
+#[derive(Debug)]
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+#[derive(Debug)]
+enum Paren {
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+enum Token {
+    Literal(String),
+    Operator(Operation),
+    Paren(Paren)
+}
+
+enum State {
+    Init,
+    End,
+    Num,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut expression = String::new();
+    
+    io::stdin()
+        .read_line(&mut expression)
+        .expect("Failed to read line.");
+
+    println!("Input an expression: {expression}");    
 }
